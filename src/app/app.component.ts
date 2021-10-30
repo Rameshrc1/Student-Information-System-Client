9f859e6357b931353a811bd4c13ccd7770637da3
Rameshrc1 <rameshrc0625@gmail.com>
2021-10-31 03:07:17 +0530

    Creat tables and get data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StudentService} from "./services/student.service";
+import {Student} from "./entities/student";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'student';
+export class AppComponent implements OnInit{
+  students: Student[] = [];
+
+  idField = new FormControl();
+  nameField = new FormControl();
+  cityField = new FormControl();
+  mobileField = new FormControl();
+
+
+  constructor(private studentService: StudentService) {
+  }
+
+  ngOnInit() {
+    this.students = this.studentService.getAll();
+
+  }
+
+  submit() : void {
+    let newStudent: Student = new Student();
+    newStudent.id = this.idField.value;
+    newStudent.name = this.nameField.value;
+    newStudent.city = this.cityField.value;
+    newStudent.mobile = this.mobileField.value;
+
+    this.idField.reset();
+    this.nameField.reset();
+    this.cityField.reset();
+    this.mobileField.reset();
+    this.students.push(newStudent);
+  }
+
+  /*
+  formControls --> Attribute - value
+                   Methods - setValue(), reset()
+   */
 }
---
+++ src/app/entities/student.ts
@@ -0,0 +1,7 @@
+export class Student{
+  id!: number;
+  name!: string;
+  city!: string;
+  mobile!: string;
+
+}
